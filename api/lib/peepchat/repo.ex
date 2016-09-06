5b7d2ce58079495ffab34818805bae7c10a1554a
Jeric Canatuan <iwaseken06@gmail.com>
2016-09-06 13:41:53 +0800

    PeepChat UI

--- api/lib/peepchat/repo.ex
+++
@@ -1,3 +0,0 @@
-defmodule Peepchat.Repo do
-  use Ecto.Repo, otp_app: :peepchat
-end
--- api/web/controllers/session_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule Peepchat.SessionController do
-  use Peepchat.Web, :controller
-
-  def index(conn, _params) do
-    # Return some static JSON for now
-    conn
-    |> json(%{status: "Ok"})
-  end
-end
